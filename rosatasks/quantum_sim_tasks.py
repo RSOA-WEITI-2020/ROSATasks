fe164cb4aa46a866fccf1d2a18bd788864c1c32d
Wojciech Gruszka <wojciech@gruszka.eu>
2020-06-24 22:35:50 +0200

    State update

--- rosatasks/quantum_sim_tasks.py
+++ rosatasks/quantum_sim_tasks.py
@@ -1,20 +1,27 @@
 
 import numpy as np
 import qiskit
+import time
 from qiskit import (
     QuantumCircuit,
     execute,
     Aer,
 )
-from celery import shared_task
+from celery import (shared_task, current_task)
 
 
 @shared_task
 def simulate_code(id, code, shots):
+    second_counter = 0
     try:
         sim = Aer.get_backend('qasm_simulator')
         c = QuantumCircuit.from_qasm_str(code)
         job = execute(c, sim, shots=shots)
+        while not job.in_final_state():
+            time.sleep(1)
+            second_counter += 1
+            current_task.update_state(state='PROGRESS', meta={
+                                      'seconds': second_counter})
         res = job.result()
         counts = res.get_counts(c)
         res = {
